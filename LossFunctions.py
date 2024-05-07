Bug fix made and first training done. For 100 epoch average loss reduced from 700 to 5

LossFunctions.py
@@ -19,6 +19,7 @@ def selection_module_loss(user_emb, item_emb, attention_probabilities):
 # privacy_preference: tensor of privacy preferences, shape: (batch_size)
 # mask: tensor of mask for padding, shape: (batch_size, num_items)
 def generation_module_loss(user_emb, item_emb, replacement_item_emb, item_emb_distances, privacy_preference, mask=None):
+    # PRIVACY LOSS
     # Maximum and minimum of distances
     min_distances = torch.min(item_emb_distances, dim=2).values  # shape: (batch_size, num_items)
     max_distances = torch.max(item_emb_distances, dim=2).values  # shape: (batch_size, num_items)
@@ -38,23 +39,24 @@ def generation_module_loss(user_emb, item_emb, replacement_item_emb, item_emb_di
 
     privacy_loss = privacy_loss.sum()
 
+    # UTILITY LOSS
     # Take the dot product of user embeddings and item embeddings
     user_item_distances = torch.einsum('ijk,ijk->ij', user_emb.unsqueeze(1), item_emb)  # shape: (batch_size, num_items)
 
     # Apply the sigmoid function
-    probs = torch.sigmoid(user_item_distances)
+    utility_loss = torch.sigmoid(user_item_distances)
 
     # Compute the negative log of the probabilities
-    log_probs = -torch.log(probs + 1e-10)  # Adding a small value to avoid log(0)
+    utility_loss = -torch.log(utility_loss + 1e-10)  # Adding a small value to avoid log(0)
 
     # Apply mask if provided
     if mask is not None:
-        log_probs = log_probs * mask
+        utility_loss = utility_loss * mask
 
     # Sum over all user-item pairs
-    utility_loss = log_probs.sum()
+    utility_loss = utility_loss.sum()
 
-    return privacy_loss + utility_loss
+    return privacy_loss, utility_loss
 
 
 

SecureLightGCN.py
@@ -31,14 +31,16 @@ class SecureLightGCN(nn.Module):
     # interacted_item_indices: tensor of interacted item indices, shape: (batch_size, num_items)
     # privacy_preferences: tensor of privacy preferences, shape: (batch_size)
     # mask: tensor of mask for padding, shape: (batch_size, num_items
-    # Returns user_emb, item_emb, attention_probabilities, replacement_item_embeddings
+    # Returns user_emb, item_emb, item_emb_distances, attention_probabilities, replacement_item_embeddings
     def forward(self, user_indices, interacted_item_indices, privacy_preferences, mask=None):
         # SELECTION MODULE
         # Get initial embeddings
         user_emb = self.user_embedding(user_indices)  # shape: (batch_size, embedding_dim)
         item_emb = self.item_embedding(interacted_item_indices)  # shape: (batch_size, num_items, embedding_dim)
 
-        # No need to duplicate user_emb as broadcasting will handle it in operations
+        # Calculate item distances (cosine similarity of item_emb with every other item embeddings)
+        item_emb_distances = torch.matmul(item_emb, self.item_embedding.weight.T)  # shape: (batch_size, num_items, num_all_items)
+
         # Combine user_emb with each item_emb
         user_emb_expanded = user_emb.unsqueeze(1)  # Add 1 to the middle for broadcasting, shape: (batch_size, 1, embedding_dim)
         combined_emb = torch.cat((user_emb_expanded.expand_as(item_emb), item_emb), dim=2)  # shape: (batch_size, num_items, 2*embedding_dim)
@@ -66,16 +68,14 @@ class SecureLightGCN(nn.Module):
         similarity_score = torch.matmul(latent_feature, self.item_embedding.weight.T)  # shape: (batch_size, num_items, num_all_items)
 
         # Suppress the similarity score of interacted items
-        mask = torch.zeros_like(similarity_score)
-        for batch_idx, indices in enumerate(interacted_item_indices):
-            mask[batch_idx, :, indices] = -float('inf')
-        similarity_score = similarity_score + mask
+        item_mask = torch.zeros_like(similarity_score)
+        for batch_idx, (indices, indices_mask) in enumerate(zip(interacted_item_indices, mask)):
+            indices = indices[indices_mask]
+            item_mask[batch_idx, :, indices] = -float('inf')
+        similarity_score = similarity_score + item_mask
 
         # Most similar item selection (Item generation)
         hard_similarity_score = F.gumbel_softmax(similarity_score, dim=-1, hard=True)  # shape: (batch_size, num_items, num_all_items)
         replacement_item_embeddings = torch.matmul(hard_similarity_score, self.item_embedding.weight)  # shape: (batch_size, num_items, embedding_dim)
 
-        # Calculate item distances
-        item_emb_distances = torch.matmul(item_emb, self.item_embedding.weight.T)  # shape: (batch_size, num_items, num_all_items)
-
-        return user_emb, item_emb, attention_probabilities, replacement_item_embeddings, item_emb_distances
+        return user_emb, item_emb, item_emb_distances, attention_probabilities, replacement_item_embeddings

main.py
@@ -1,21 +1,24 @@
 from DataLoader import load_data, get_user_item_interaction, UserItemDataset, collate_fn
 from SecureLightGCN import SecureLightGCN
-from LossFunctions import selection_module_loss, generation_module_loss
+import LossFunctions
 import torch
 from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
 
-
 # Constants
+train_test_ratio = 0.9
 embedding_dim = 32
 replacement_ratio = 0.1
 privacy_preference = 0.5
+num_epochs = 100
+batch_size = 64
 
+# Set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load training data
 train_file_path = 'data/clothing/data.txt'
 data = load_data(train_file_path)
-data = data[:10]
 
 # Get user and item interaction data
 user_ids, interacted_item_ids, user_id_count, item_id_count = get_user_item_interaction(data)
@@ -24,64 +27,52 @@ print("Number of items:", item_id_count)
 
 # Create DataLoader
 dataset = UserItemDataset(user_ids, interacted_item_ids, [privacy_preference] * len(user_ids))
-train_count = int(0.9 * len(dataset))
+train_count = int(train_test_ratio * len(dataset))
 test_count = len(dataset) - train_count
 train_dataset, test_dataset = random_split(dataset, [train_count, test_count])
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, collate_fn=collate_fn)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, collate_fn=collate_fn)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
 
 # Initialize the model
-model = SecureLightGCN(user_id_count, item_id_count, embedding_dim)
+model = SecureLightGCN(user_id_count, item_id_count, embedding_dim).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-# Training loop
-num_epochs = 5  # Set the number of epochs
-model.train()
-
+# Iterate over each epoch
 for epoch in range(num_epochs):
-    total_loss = 0
-    # Wrap your data loader with tqdm for a progress bar
+    # Wrap data loader with tqdm for a progress bar
     progress_bar = tqdm(train_loader, desc=f'Epoch {epoch+1}/{num_epochs}', leave=True)
+
+    # Iterate over each batch
+    total_loss = 0
     for user_ids_batch, items_ids_batch, privacy_preferences, mask in progress_bar:
+        # Move data to device
+        user_ids_batch = user_ids_batch.to(device)
+        items_ids_batch = items_ids_batch.to(device)
+        privacy_preferences = privacy_preferences.to(device)
+        mask = mask.to(device)
+
+        # Zero the gradients
         optimizer.zero_grad()
-        user_emb, item_emb, attention_probabilities, replacement_item_embeddings, item_emb_distances = model(user_ids_batch, items_ids_batch, privacy_preferences, mask)
-        selection_module_loss = selection_module_loss(user_emb, item_emb, attention_probabilities)
-        generation_module_loss = generation_module_loss(user_emb, item_emb, replacement_item_embeddings, item_emb_distances, privacy_preferences, mask)
-        loss = selection_module_loss + generation_module_loss
+
+        # Forward pass
+        user_emb, item_emb, item_emb_distances, attention_probabilities, replacement_item_embeddings = model(user_ids_batch, items_ids_batch, privacy_preferences, mask)
+
+        # Calculate the loss
+        selection_module_loss = LossFunctions.selection_module_loss(user_emb, item_emb, attention_probabilities)
+        generation_module_privacy_loss, generation_module_utility_loss = LossFunctions.generation_module_loss(user_emb, item_emb, replacement_item_embeddings, item_emb_distances, privacy_preferences, mask)
+        loss = 20000 * selection_module_loss + 50 * generation_module_privacy_loss + generation_module_utility_loss
+
+        # Backward pass
         loss.backward()
         optimizer.step()
+
+        # Accumulate the total loss
         total_loss += loss.item()
 
-        # Optionally update the progress bar with the current loss
+        # Update the progress bar with the current loss
         progress_bar.set_postfix({'loss': loss.item()})
 
+    # Calculate the average loss for the epoch
     average_loss = total_loss / len(train_loader)
     progress_bar.close()
     print(f"Epoch {epoch+1}, Average Loss: {average_loss}")
-
-# # Calculate hit rate or precision of top-k recommendations
-# hits = 0
-# total = 0
-# k = 10  # Top-k items
-# model.eval()  # Make sure the model is in eval mode
-# with torch.no_grad():  # Disable gradient computation during inference
-#     for user_ids_batch, items_ids_batch, mask in test_loader:
-#         attention_scores = model(user_ids_batch, items_ids_batch, mask)  # Get model outputs in inference
-#
-#         # TODO: It should not look for top atentions. It should look for most similar item. Fix this
-#         k = min(k, mask.sum(dim=1).min().item())  # Adjust k to the number of valid items in the batch
-#         _, top_k_indices = torch.topk(attention_scores, k, dim=1, largest=True, sorted=True)
-#
-#         for i, user_id in enumerate(user_ids_batch):
-#             actual_items = set(items_ids_batch[i][mask[i]].tolist())  # Apply mask to actual items
-#             recommended_items = set(top_k_indices[i].tolist())
-#
-#             if actual_items.intersection(recommended_items):
-#                 hits += 1
-#             total += 1
-#             print(actual_items)
-#             print(recommended_items)
-#             print()
-#
-# precision_at_k = hits / total
-# print(f"Precision@{k}: {precision_at_k}")